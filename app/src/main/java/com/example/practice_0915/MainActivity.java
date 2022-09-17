17bd57db9140f435712d096789f79b6d6191e27b
Jung-H-C <xotourllife@naver.com>
2022-09-17 19:53:22 +0900

    First Commit

--- app/src/main/java/com/example/practice_0915/MainActivity.java
+++ app/src/main/java/com/example/practice_0915/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.practice_0915;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button1 = (Button) findViewById(R.id.button1); // xml 버튼을 java 버튼이랑 id로 연결.
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getApplicationContext(), "버튼을 눌렀어요",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
